480fdc8a87ebaba4f0da944c2a72bad7c29f8a0f
SYSTEX\2200531 <2200531@systex.com>
2023-03-15 15:59:33 +0800

    OnlyDotNetApi

---
+++ WebApplication1/Controllers/SimpleAnswerController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SimpleAnswerController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "成功!";
+        }
+    }
+}
